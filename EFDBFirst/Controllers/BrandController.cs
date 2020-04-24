68b065728834fca613fde9a5ec30e732551b3596
Lakshman540 <sriteja540@gmail.com>
2020-04-24 18:46:48 +0530

    Final Commit

---
+++ EFDBFirst/Controllers/BrandController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDBFirst.Models;
+
+namespace EFDBFirst.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Brand> brands = db.Brands.ToList();
+            return View(brands);
+        }
+    }
+}
---
+++ EFDBFirst/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDBFirst.Models;
+
+namespace EFDBFirst.Controllers
+{
+    public class CategoryController : Controller
+    {
+       
+        // GET: Category
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Category> categories= db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
